f933bafc09eeb37570be3058b8bbae750849a485
Reem Joudeh <joudehreem@gmail.com>
2024-05-29 10:02:51 +0300

    changes

--- functions_intermediate1.py
+++ functions_intermediate1.py
@@ -10,8 +10,6 @@ def randInt(min=0, max=100):
     
     num = random.random()*(max - min) + min
     return round(num)
-
-# Test cases
 print(randInt())                  # should print a random integer between 0 to 100
 print(randInt(max=50))            # should print a random integer between 0 and 50
 print(randInt(min=50))            # should print a random integer between 50 and 100
